ca91e46ab50f944dc489cae1b35b91fcafe0fb47
Brandon Goodson <goodsonwebdesign@users.noreply.github.com>
2025-05-28 22:12:34 -0400

    refactor(user.effects): switch to functional injection for improved dependency management

--- src/app/store/effects/user.effects.ts
+++ src/app/store/effects/user.effects.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { UserActions } from '../actions/user.actions';
 import { UsersService } from '../../core/services/users/users.service';
 import { catchError, map, mergeMap, of, switchMap } from 'rxjs';
 
-@Injectable({ providedIn: 'root' })
+@Injectable()
 export class UserEffects {
-  constructor(private actions$: Actions, private usersService: UsersService) {}
+  // Use functional injection pattern
+  private actions$ = inject(Actions);
+  private usersService = inject(UsersService);
 
   loadUsers$ = createEffect(() =>
     this.actions$.pipe(
